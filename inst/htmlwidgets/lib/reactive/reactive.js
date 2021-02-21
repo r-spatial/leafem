9db681e08fec7d8801f4c299c56eb661d6fefb9b
tim-salabim <tim.appelhans@gmail.com>
2021-02-21 12:51:00 +0100

    proper resetting of style for bindto_layer on out action of bind_layer. #38

--- inst/htmlwidgets/lib/reactive/reactive.js
+++ inst/htmlwidgets/lib/reactive/reactive.js
@@ -36,13 +36,19 @@ LeafletWidget.methods.addReactiveLayer = function(x,
     }
 
     var bindto_layer = map.layerManager._byGroup[bindTo];
+
+    if (bindto_layer === undefined) {
+        return;
+    }
+
     var bindto_layer_key = Object.keys(bindto_layer);
 
     var bind_layer = L.geoJSON(x, {
       pointToLayer: function (feature, latlng) {
           return L.circleMarker(latlng, options);
       },
-      style: style
+      style: style,
+      interactive: true
     });
 
     if (pop !== null) {
@@ -89,22 +95,37 @@ LeafletWidget.methods.addReactiveLayer = function(x,
           }
         });
 
-        e.target.eachLayer(function (layer) {
-          layer.setStyle(layer.defaultOptions.style(layer.feature));
-        });
+        e.layer.setStyle(e.layer.defaultOptions.style(e.layer.feature));
+
+        //e.target.eachLayer(function (layer) {
+          //layer.setStyle(layer.defaultOptions.style(layer.feature));
+        //});
 
       }
     });
 
-    bind_layer.on(out, function (e) {
+    bind_layer
+    .on(out, function (e) {
       if (e.originalEvent.ctrlKey) {
         // console.log(e.layer.feature.properties[by]);
         var cur_by = e.layer.feature.properties[by];
         var ids = getAllIndexes(nkeys, cur_by);
-
+        var bindto_layer = map.layerManager._byGroup[bindTo];
+        var bindto_current = bindto_layer[Object.keys(bindto_layer)];
+        var okeys_current = Object.keys(bindto_current._layers);
+        var nkeys_current = [...okeys_current];
+        nkeys_current.forEach( (key, i, self) => self[i] = bindto_current._layers[key].feature.properties[by] );
+        var ids_current = getAllIndexes(nkeys_current, cur_by);
+//debugger;
         ids.forEach(function(i) {
           if (map.hasLayer(bind_layer._layers[okeys[i]])) {
             map.removeLayer(bind_layer._layers[okeys[i]]);
+          }
+        });
+
+        ids_current.forEach(function(i) {
+          if (nkeys_current[i] == cur_by) {
+            bindto_current._layers[okeys_current[i]].setStyle(bindto_current._layers[okeys_current[i]].defaultOptions.style(bindto_current._layers[okeys_current[i]].feature));
           }
         });
       }

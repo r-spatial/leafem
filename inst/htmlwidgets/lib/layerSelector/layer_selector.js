e1b35d7d6efc9d95175f1db3075f0e8457f11776
Tim Appelhans <tim.appelhans@metergroup.com>
2023-02-25 19:29:10 +0100

    strings working :-)

--- inst/htmlwidgets/lib/layerSelector/layer_selector.js
+++ inst/htmlwidgets/lib/layerSelector/layer_selector.js
@@ -47,16 +47,35 @@ updateLayerStyle = function(layerId) {
   for (var i = 0; i < layer_keys.length; i++) {
     vals[i] = layer._layers[layer_keys[i]].feature.properties[colname]
   }
-  mn = Math.min(...vals);
-  mx = Math.max(...vals);
-  var col = chroma.scale("YlGnBu").domain([mn, mx]);
+
+  let colorFun = colFunc(vals);
 
   layer.eachLayer(function(layer) {
     console.log(layer.feature.properties[colname]);
     if (colname === "---choose layer---") {
       layer.setStyle(layer.defaultOptions.style(layer.feature));
     } else {
-      layer.setStyle({fillColor: col(layer.feature.properties[colname]), fillOpacity: 0.9})
+      layer.setStyle({fillColor: colorFun(layer.feature.properties[colname]), fillOpacity: 0.9})
     }
   });
 };
+
+colFunc = function(values) {
+
+  let col;
+
+  if (typeof(values[0]) === 'number') {
+    mn = Math.min(...values);
+    mx = Math.max(...values);
+    col = chroma.scale("YlOrRd").domain([mn, mx]);
+  } else if (typeof(values[0]) === 'string') {
+    //var arr = ["c", "a", "b", "b"];
+    let unique = [...new Set(values)];
+    clrs = chroma.scale("Set1").colors(unique.length);
+    var clrArr = Object.fromEntries(unique.map((key, index)=> [key, clrs[index]]));
+    col = function(val) {
+      return clrArr[val];
+    }
+  }
+  return col;
+}

bc081c7c9626d8068f0a9a36d984c9c23d18a801
Sebastian Gatscha <gatscha@trafficon.eu>
2024-05-29 08:39:43 +0200

    fgb attach by group fix: id can be single string or property name feat: expose highlightOptions fix: label can be a function

--- inst/htmlwidgets/lib/FlatGeoBuf/fgb.js
+++ inst/htmlwidgets/lib/FlatGeoBuf/fgb.js
@@ -12,7 +12,7 @@ function mouseHandler(mapId, layerId, group, eventName, extraInfo) {
     }
     let eventInfo = $.extend(
       {
-        id: layerId,
+        id: (e.layer.feature.properties[layerId]?.toString() ?? layerId.toString()),
         ".nonce": Math.random()  // force reactivity
       },
       group !== null ? {group: group} : null,
@@ -33,11 +33,12 @@ LeafletWidget.methods.addFlatGeoBuf = function (layerId,
                                                 options,
                                                 className,
                                                 scale,
-                                                scaleFields) {
+                                                scaleFields,
+                                                highlightOptions) {
 
-  var map = this;
-  var gl = false;
-  var pane;
+  const map = this;
+  let gl = false;
+  let pane;
 
   if (options === null || options.pane === undefined) {
     pane = 'overlayPane';
@@ -45,7 +46,7 @@ LeafletWidget.methods.addFlatGeoBuf = function (layerId,
     pane = options.pane;
   }
 
-  var data_fl = document.getElementById(layerId + '-1-attachment');
+  let data_fl = document.getElementById(group + '-1-attachment');
 
   if (data_fl === null) {
     data_fl = url;
@@ -53,8 +54,8 @@ LeafletWidget.methods.addFlatGeoBuf = function (layerId,
     data_fl = data_fl.href;
   }
 
-  var popUp;
-  var colnames = [];
+  let popUp;
+  let colnames = [];
 
   function handleHeaderMeta(headerMeta) {
     headerMeta.columns.forEach(function(col) {
@@ -66,7 +67,8 @@ LeafletWidget.methods.addFlatGeoBuf = function (layerId,
     // use fgb JavaScript API to iterate stream into results (features as geojson)
     // NOTE: would be more efficient with a special purpose Leaflet deserializer
     let it = flatgeobuf.deserialize(response.body, undefined, handleHeaderMeta);
-    var cntr = 0;
+    let cntr = 0;
+
     // handle result
     function handleResult(result) {
         if (!result.done) {
@@ -86,9 +88,8 @@ LeafletWidget.methods.addFlatGeoBuf = function (layerId,
               pop = null;
             }
 
-            if (scaleFields === null &
-                result.value.properties !== undefined) {
-              var vls = Object.values(style);
+            if (scaleFields === null & result.value.properties !== undefined) {
+              let vls = Object.values(style);
               scaleFields = [];
               vls.forEach(function(name) {
                 //if (name in colnames) {
@@ -100,8 +101,7 @@ LeafletWidget.methods.addFlatGeoBuf = function (layerId,
               });
             }
 
-            lyr = L.geoJSON(result.value, Object.assign(
-              {
+            lyr = L.geoJSON(result.value, Object.assign({
                 pointToLayer: function (feature, latlng) {
                   return L.circleMarker(latlng, options);
                 },
@@ -120,33 +120,57 @@ LeafletWidget.methods.addFlatGeoBuf = function (layerId,
                   return layer.feature.properties[label].toString();
                 }, {sticky: true});
               } else if (typeof(label) === Object || (typeof(label) === 'object' && label.length > 1)) {
-                var lb = label[cntr];
+                let lb = label[cntr];
                 lyr.bindTooltip(function (layer) {
                   return(lb);
                 }, {sticky: true});
+              } else if (typeof(label) === "function") {
+                lyr.bindTooltip(label, {sticky: true});
               } else {
                 lyr.bindTooltip(function (layer) {
                   return(label);
                 }, {sticky: true});
               }
             }
+
+            if (highlightOptions && typeof highlightOptions === 'object' && Object.keys(highlightOptions).length > 0) {
+              lyr.on({
+                // highlight on hover
+                'mouseover': function(e) {
+                    const layer = e.target;
+                    layer.setStyle(highlightOptions);
+                    if (highlightOptions.bringToFront) {
+                      layer.bringToFront();
+                    }
+                },
+                // remove highlight when hover stops
+                'mouseout': function(e) {
+                    const layer = e.target;
+                    layer.setStyle(style);
+                    if (highlightOptions.sendToBack) {
+                      layer.bringToBack();
+                    }
+                }
+              })
+            }
+
             lyr.on("click", mouseHandler(map.id, layerId, group, "shape_click"));
             lyr.on("mouseover", mouseHandler(map.id, layerId, group, "shape_mouseover"));
             lyr.on("mouseout", mouseHandler(map.id, layerId, group, "shape_mouseout"));
             map.layerManager.addLayer(lyr, null, null, group);
+
             it.next().then(handleResult);
           }
         }
+
         cntr += 1;
     }
     it.next().then(handleResult);
   }
 
   fetch(data_fl) //, {mode: 'no-cors'})
-  .then(handleResponse);
+    .then(handleResponse);
 
-  //map.fitBounds(lyr.getBounds());
-  //map.layerManager.addLayer(layer, null, null, group);
 };
 
 function makePopup(popup, className) {
@@ -171,7 +195,7 @@ function makePopup(popup, className) {
   } else if (typeof(popup) === "object") {
     pop = function(feature, layer) {
       if (feature.properties.mvFeatureId !== undefined) {
-        var idx = feature.properties.mvFeatureId;
+        let idx = feature.properties.mvFeatureId;
         layer.bindPopup(popup[idx - 1], { maxWidth: 2000 });
       }
       if (feature.properties.mvFeatureId === undefined) {
@@ -193,18 +217,14 @@ function makePopup(popup, className) {
 
 
 function json2table(json, cls) {
-  var cols = Object.keys(json);
-  var vals = Object.values(json);
-
-  var tab = "";
-
+  let cols = Object.keys(json);
+  let vals = Object.values(json);
+  let tab = "";
   for (let i = 0; i < cols.length; i++) {
     tab += "<tr><th>" + cols[i] + "&emsp;</th>" +
     "<td align='right'>" + vals[i] + "&emsp;</td></tr>";
   }
-
   return "<table class=" + cls + ">" + tab + "</table>";
-
 }
 
 
@@ -219,31 +239,24 @@ function json2table(json, cls) {
  * @return {Object}       The new object
  */
 function pick(obj, props) {
-
 	'use strict';
-
 	// Make sure object and properties are provided
 	if (!obj || !props) return;
-
 	// Create new object
-	var picked = {};
-
+	let picked = {};
 	// Loop through props and push to new object
 	props.forEach(function(prop) {
 		picked[prop] = obj[prop];
 	});
-
-	// Return new object
 	return picked;
-
 }
 
 
 function updateStyle(style_obj, feature, scale, scaleValues) {
-  var cols = Object.keys(style_obj);
-  var vals = Object.values(style_obj);
+  let cols = Object.keys(style_obj);
+  let vals = Object.values(style_obj);
 
-  var out = {};
+  let out = {};
 
   for (let i = 0; i < cols.length; i++) {
     if (vals[i] === null) {
@@ -279,21 +292,22 @@ function rescale(value, to_min, to_max, from_min, from_max) {
 
 
 LeafletWidget.methods.addFlatGeoBufFiltered = function (layerId,
-                                                 group,
-                                                 url,
-                                                 popup,
-                                                 label,
-                                                 style,
-                                                 options,
-                                                 className,
-                                                 scale,
-                                                 scaleFields,
-                                                 minZoom,
-                                                 maxZoom) {
+                                                        group,
+                                                        url,
+                                                        popup,
+                                                        label,
+                                                        style,
+                                                        options,
+                                                        className,
+                                                        scale,
+                                                        scaleFields,
+                                                        minZoom,
+                                                        maxZoom,
+                                                        highlightOptions) {
 
-  var map = this;
-  var gl = false;
-  var pane;
+  const map = this;
+  let gl = false;
+  let pane;
 
   if (options === null || options.pane === undefined) {
     pane = 'overlayPane';
@@ -301,7 +315,7 @@ LeafletWidget.methods.addFlatGeoBufFiltered = function (layerId,
     pane = options.pane;
   }
 
-  var data_fl = document.getElementById(layerId + '-1-attachment');
+  let data_fl = document.getElementById(group + '-1-attachment');
 
   if (data_fl === null) {
     data_fl = url;
@@ -309,8 +323,8 @@ LeafletWidget.methods.addFlatGeoBufFiltered = function (layerId,
     data_fl = data_fl.href;
   }
 
-  var popUp;
-  var colnames = [];
+  let popUp;
+  let colnames = [];
 
   function handleHeaderMeta(headerMeta) {
     headerMeta.columns.forEach(function(col) {
@@ -329,94 +343,119 @@ LeafletWidget.methods.addFlatGeoBufFiltered = function (layerId,
       };
   }
 
+  // Initialize previousResults/nextResults if not already initialized
   var previousResults = previousResults || {};
-  previousResults[layerId] = L.layerGroup();
-  map.layerManager.addLayer(previousResults[layerId], null, layerId, group);
+  previousResults[group] = L.layerGroup();
+  map.layerManager.addLayer(previousResults[group], null, layerId, group);
 
+  // Async Update Function
   async function updateResults() {
-      var nextResults = nextResults || {};
-      nextResults[layerId] = L.layerGroup();
-      map.layerManager.addLayer(nextResults[layerId], null, layerId, group);
-      // remove the old results
-      map.layerManager.removeLayer(previousResults[layerId], layerId);
-      // previousResults[layerId].remove();
-      previousResults[layerId] = nextResults[layerId];
 
-      // Use flatgeobuf JavaScript API to iterate features as geojson.
-      // Because we specify a bounding box, flatgeobuf will only fetch the resubset of data,
-      // rather than the entire file.
-      const iter = flatgeobuf.deserialize(data_fl, fgBoundingBox(), handleHeaderMeta);
+    // remove the old results
+    map.layerManager.removeLayer(previousResults[group], layerId);
+    previousResults[group].remove()
 
-    if (map.getZoom() >= minZoom & map.getZoom() <= maxZoom & map.hasLayer(previousResults[layerId])) {
-      for await (const feature of iter) {
-            if (popup) {
-              pop = makePopup(popup, className);
+    // add next results
+    var nextResults = nextResults || {};
+    nextResults[group] = L.layerGroup();
+    map.layerManager.addLayer(nextResults[group], null, layerId, group);
+
+    // Replace previous with next
+    previousResults[group] = nextResults[group];
+
+    // Use flatgeobuf JavaScript API to iterate features as geojson.
+    // Because we specify a bounding box, flatgeobuf will only fetch the relevant subset of data,
+    // rather than the entire file.
+    if (map.getZoom() >= minZoom & map.getZoom() <= maxZoom & map.hasLayer(previousResults[group])) {
+
+      let iter = flatgeobuf.deserialize(data_fl, fgBoundingBox(), handleHeaderMeta);
+
+      for await (let feature of iter) {
+
+        if (popup) {
+          pop = makePopup(popup, className);
+        } else {
+          pop = null;
+        }
+
+        if (scaleFields === null & feature.properties !== undefined) {
+          let vls = Object.values(style);
+          scaleFields = [];
+          vls.forEach(function(name) {
+            //if (name in colnames) {
+            if (colnames.includes(name)) {
+              scaleFields.push(true);
             } else {
-              pop = null;
+              scaleFields.push(false);
             }
+          });
+        }
 
-          if (scaleFields === null &
-                feature.properties !== undefined) {
-              var vls = Object.values(style);
-              scaleFields = [];
-              vls.forEach(function(name) {
-                //if (name in colnames) {
-                if (colnames.includes(name)) {
-                  scaleFields.push(true);
-                } else {
-                  scaleFields.push(false);
+        lyr = L.geoJSON(feature, {
+          pointToLayer: function (feature, latlng) {
+            return L.circleMarker(latlng, options);
+          },
+          style: function(feature) {
+            return updateStyle(style, feature, scale, scaleFields);
+          },
+          onEachFeature: pop,
+          pane: pane
+        });
+
+        if (label) {
+          if (Object.keys(feature.properties).includes(label)) {
+            lyr.bindTooltip(function (layer) {
+              return layer.feature.properties[label].toString();
+            }, {sticky: true});
+          } else if (typeof(label) === "function") {
+            lyr.bindTooltip(label, {sticky: true});
+          } else {
+            lyr.bindTooltip(function (layer) {
+              return(label);
+            }, {sticky: true});
+          }
+        }
+
+        if (highlightOptions && typeof highlightOptions === 'object' && Object.keys(highlightOptions).length > 0) {
+          lyr.on({
+            // highlight on hover
+            'mouseover': function(e) {
+                const layer = e.target;
+                layer.setStyle(highlightOptions);
+                if (highlightOptions.bringToFront) {
+                  layer.bringToFront();
                 }
-              });
+            },
+            // remove highlight when hover stops
+            'mouseout': function(e) {
+                const layer = e.target;
+                layer.setStyle(style);
+                if (highlightOptions.sendToBack) {
+                  layer.bringToBack();
+                }
             }
+          })
+        }
 
-          lyr = L.geoJSON(feature, {
-              pointToLayer: function (feature, latlng) {
-                  return L.circleMarker(latlng, options);
-              },
-              style: function(feature) {
-                return updateStyle(style, feature, scale, scaleFields);
-              },
-              onEachFeature: pop,
-              pane: pane
-            });
-
-            if (label) {
-              if (Object.keys(feature.properties).includes(label)) {
-                lyr.bindTooltip(function (layer) {
-                  return layer.feature.properties[label].toString();
-                }, {sticky: true});
-              } else if (typeof(label) === Object || (typeof(label) === 'object' && label.length > 1)) {
-                var lb = label[cntr];
-                lyr.bindTooltip(function (layer) {
-                  return(lb);
-                }, {sticky: true});
-              } else {
-                lyr.bindTooltip(function (layer) {
-                  return(label);
-                }, {sticky: true});
-              }
-            }
-
-         lyr.on("click", mouseHandler(map.id, layerId, group, "shape_click"));
-         lyr.on("mouseover", mouseHandler(map.id, layerId, group, "shape_mouseover"));
-         lyr.on("mouseout", mouseHandler(map.id, layerId, group, "shape_mouseout"));
-         lyr.addTo(nextResults[layerId]);
+        lyr.on("click", mouseHandler(map.id, layerId, group, "shape_click"));
+        lyr.on("mouseover", mouseHandler(map.id, layerId, group, "shape_mouseover"));
+        lyr.on("mouseout", mouseHandler(map.id, layerId, group, "shape_mouseout"));
+        lyr.addTo(nextResults[group]);
       }
     }
   }
-  // if the user is panning around alot, only update once per second max
-  // updateResults = _.throttle(updateResults, 1000);
 
   // show results based on the initial map
   updateResults();
-  // ...and update the results whenever the map moves
+
+  // update the results whenever the map moves or the layer is added again
   map.on("moveend", function(s) {
-      //rectangle.setBounds(getBoundForRect());
-      updateResults();
+    updateResults();
   });
   map.on('layeradd', function(event) {
-     if(event.layer == previousResults[layerId]) {
-         updateResults();
-     }
-});
+    if (event.layer == previousResults[group]) {
+      updateResults();
+    }
+  });
+
 };

2244491d87d9a24599fb72d02f5cdbaecccce3ba
Sebastian Gatscha <gatscha@trafficon.eu>
2025-04-22 12:01:10 +0200

    rm |> from examples otherwise pkg depends on R > 4.1, polish News for check NOTEs

--- R/addStarsImage.R
+++ R/addStarsImage.R
@@ -6,6 +6,7 @@
 #' @param data the data object from which the argument values are derived; by
 #'   default, it is the \code{data} object provided to \code{leaflet()}
 #'   initially, but can be overridden.
+#' @inheritParams addRasterRGB
 #' @inheritParams leaflet::addRasterImage
 #' @param ... currently not used.
 #'
